cd6b648400492bfb9f0bfbe9cfe69441c68bf4a1
katsumi143 <32640219+katsumi143@users.noreply.github.com>
2023-08-05 22:59:43 +1000

    version 1.2.0

--- app/route.ts
+++ app/route.ts
@@ -2,5 +2,5 @@ import { json } from '../lib/response';
 export const runtime = 'edge';
 export const GET = () => json({
 	name: 'voxelified-rest-api',
-	version: '1.1.0' // TODO acquire version from package.json
+	version: '1.2.0' // TODO acquire version from package.json
 });
