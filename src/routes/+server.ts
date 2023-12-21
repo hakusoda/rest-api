9f695f83571d7c10d5f5e0ecbd5699c364ceabf1
katsumi143 <32640219+katsumi143@users.noreply.github.com>
2023-12-21 20:27:52 +1000

    update versioning scheme

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,7 +1,17 @@
 import { json } from '@sveltejs/kit';
 
-import type { RequestHandler } from './$types';
-export const GET = (() => json({
+import { version } from '../../package.json';
+import { VERCEL_GIT_REPO_SLUG, VERCEL_GIT_REPO_OWNER, VERCEL_GIT_COMMIT_SHA, VERCEL_GIT_COMMIT_REF, VERCEL_GIT_COMMIT_AUTHOR_LOGIN } from '$env/static/private';
+
+const repository = `${VERCEL_GIT_REPO_OWNER}/${VERCEL_GIT_REPO_SLUG}`;
+export const GET = () => json({
 	name: 'hakumi-rest-api',
-	version: '1.0.0'
-})) satisfies RequestHandler;
+	version,
+	git_commit: {
+		sha: VERCEL_GIT_COMMIT_SHA,
+		branch: VERCEL_GIT_COMMIT_REF,
+		author: VERCEL_GIT_COMMIT_AUTHOR_LOGIN,
+		repository,
+		website_url: `https://github.com/${repository}/commit/${VERCEL_GIT_COMMIT_SHA}`
+	}
+});
