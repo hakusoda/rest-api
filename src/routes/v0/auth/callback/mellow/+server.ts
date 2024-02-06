f909f30db0726ae45291f2099b7ab46d70eabc22
katsumi143 <32640219+katsumi143@users.noreply.github.com>
2024-02-06 14:33:37 +1000

    allow existing sessions for mellow callback

--- src/routes/v0/auth/callback/mellow/+server.ts
+++ src/routes/v0/auth/callback/mellow/+server.ts
@@ -8,16 +8,13 @@ import { JWT_SECRET, WEBSITE_URL, USER_CONNECTION_CALLBACKS } from '$lib/constan
 
 export async function GET({ url, locals: { getSession }, cookies }) {
 	const session = await getSession(false);
-	if (session)
-		throw error(400, 'session_mot_allowed');
-
 	const state = url.searchParams.get('state');
 	if (!state)
 		throw error(400, 'missing_state');
 
 	const { sub, name, username, avatar_url, website_url } = await USER_CONNECTION_CALLBACKS[UserConnectionType.Discord](url);
 
-	const response = await supabase.from('users')
+	const user_id = session ? session.sub : handleResponse(await supabase.from('users')
 		.insert({
 			name,
 			username: `mellow_${crypto.randomUUID()}`,
@@ -26,14 +23,14 @@ export async function GET({ url, locals: { getSession }, cookies }) {
 		})
 		.select('id')
 		.limit(1)
-		.single();
-	handleResponse(response);
+		.single()
+	).data!.id;
 
 	handleResponse(await supabase.from('user_connections')
 		.insert({
 			sub,
 			type: UserConnectionType.Discord,
-			user_id: response.data!.id,
+			user_id,
 			username,
 			avatar_url,
 			website_url,
@@ -41,17 +38,19 @@ export async function GET({ url, locals: { getSession }, cookies }) {
 		})
 	);
 
-	const token = await new SignJWT({
-		sub: response.data!.id,
-		mellow_username: username,
-		mellow_user_state: state
-	})
-		.setProtectedHeader({ alg: 'HS256' })
-		.setIssuedAt()
-		.setExpirationTime('5m')
-		.sign(JWT_SECRET);
+	if (!session) {
+		const token = await new SignJWT({
+			sub: user_id,
+			mellow_username: username,
+			mellow_user_state: state
+		})
+			.setProtectedHeader({ alg: 'HS256' })
+			.setIssuedAt()
+			.setExpirationTime('5m')
+			.sign(JWT_SECRET);
 
-	cookies.set('auth-token', token, { path: '/', domain: '.hakumi.cafe', expires: new Date(Date.now() + 300000), sameSite: 'none', httpOnly: false });
+		cookies.set('auth-token', token, { path: '/', domain: '.hakumi.cafe', expires: new Date(Date.now() + 300000), sameSite: 'none', httpOnly: false });
+	}
 
 	throw redirect(302, `${WEBSITE_URL}/mellow/finish_signup`);
 }
